062da1f21215019cd44c01aa11750e2b16dfbbfd
Franciszek Trynka <52320368+NKA-root@users.noreply.github.com>
2022-03-25 22:16:25 +0100

    Add files via upload

---
+++ ToDoList/ToDoList/Item.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Item
+    {
+        public Item(string name, int StopienPilnosci, DateTime dataKoncowa)
+        {
+            nazwa = name;
+            Pilnosc = StopienPilnosci;
+            deadline = dataKoncowa;
+        }
+        #region Operatory
+
+        public static bool operator <(Item a,Item b)
+        {
+            return (a.deadline < b.deadline);
+        }
+        public static bool operator >(Item a, Item b)
+        {
+            return (a.deadline > b.deadline);
+        }
+        public static bool operator <=(Item a, Item b)
+        {
+            return (a.deadline <= b.deadline);
+        }
+        public static bool operator >=(Item a, Item b)
+        {
+            return (a.deadline >= b.deadline);
+        }
+
+        #endregion
+        private string nazwa { get; set; }
+        public string Nazwa
+        {
+            get { return nazwa; }
+            set { if (value != "" && value != null)nazwa = value;}
+        }
+        private int pilnosc;
+
+        public int Pilnosc
+        {
+            get { return pilnosc; }
+            set { if (value < 0)
+                    pilnosc = 0;
+                else if (value > 3) pilnosc = 3;
+                else pilnosc = value;
+            }
+        }
+
+        // nice to do > potrzebne > pilne > bardzo pilne 
+        private DateTime deadline { get; set; }
+        public void PokazNaKonsoli()
+        {
+            string output;
+            switch (Pilnosc)
+            {
+                case 0:output = "nice to do"; break;
+                case 1:output = "potrzebne"; break;
+                case 2:output = "pilne"; break;
+                case 3:output = "bardzo pilne"; break;
+                default:output = "brak stopnia pilnosci";break;
+            }
+            Console.WriteLine(nazwa + " " + output + " do: " + Data());
+        }
+        public void Zmien()
+        {
+            Console.Clear();
+            char input;
+            while (true)
+            {
+                Console.WriteLine("1. Zmien nazwe, aktualna nazwa: " + nazwa);
+                Console.WriteLine("2. Zmien stopien pilnosci, aktualny: " + pilnosc);
+                Console.WriteLine("(0 - minimum, 3 - maximum)");
+                Console.WriteLine("3. Zmien deadline, aktualny: " + deadline);
+                Console.WriteLine("4. Nic ");
+                Console.WriteLine("Podaj nr czego chcesz zmienic: ");
+                input = Console.ReadLine()[0];
+                if (input == '1' || input == '2' || input == '3' || input == '4')
+                    break;
+            }
+            Console.Clear();
+            switch(input)
+            {
+                case '1':
+                    Console.WriteLine("Podaj nowa nazwe: ");
+                    Nazwa = Console.ReadLine();
+                    break;
+                case '2':
+                    Console.WriteLine("Podaj nowy stopien pilnosci(0-3): ");
+                    Pilnosc = int.Parse(Console.ReadLine());
+                    break;
+                case '3':
+                    Console.WriteLine("Podaj nowa date: ");
+                    Console.WriteLine("Dzien: "); int day = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Miesciac: "); int month = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Rok: "); int year = int.Parse(Console.ReadLine());
+                    deadline = new DateTime(year, month, day);
+                    break;
+                case '4': break;
+            }
+            Console.ReadKey();
+        }
+        public string Data()
+        {
+            return $"{deadline.Day}/{deadline.Month}/{deadline.Year}";
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDoList
+{
+    class Program
+    {
+        public static List<Item> lista = new List<Item>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+            }
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista Rzeczy do zrobienia");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1. Zobacz liste");
+            Console.WriteLine("2. Posortuj liste");
+            Console.WriteLine("3. Dodaj zadanie do listy");
+            Console.WriteLine("4. Usun zadanie z listy");
+            Console.WriteLine("5. Zmien zadanie");
+            Console.WriteLine("Wpisz nr zadania ktore chcesz wykonac: ");
+            char inChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            switch (inChar)
+            {
+                case '1':
+                    lista.ToList().ForEach(x => x.PokazNaKonsoli());
+                    Console.ReadKey();
+                    break;
+                case '2':
+                    Console.WriteLine("Posortuj liste wzgledem: ");
+                    Console.WriteLine("1. Nazwy");
+                    Console.WriteLine("2. Stopnia Pilnosci");
+                    Console.WriteLine("3. Deadline'u");
+                    char sort = Console.ReadLine()[0];
+                    Console.WriteLine("1. Rosnaco");
+                    Console.WriteLine("2. Malejaco");
+                    bool descending = Console.ReadLine()[0] == '2';
+                    switch (sort)
+                    {
+                        case '1':
+                            lista.ToList(); if (descending)
+                                lista.OrderByDescending(x => x.Nazwa);
+                            else
+                                lista.OrderBy(x => x.Nazwa);
+                            break;
+                        case '2':
+                            lista.ToList(); if (descending)
+                                lista.OrderByDescending(x => x.Pilnosc);
+                            else
+                                lista.OrderBy(x => x.Pilnosc);
+                            break;
+                        case '3':
+                            lista.ToList(); if (descending)
+                                lista.OrderByDescending(x => x);
+                            else
+                                lista.OrderBy(x => x);
+                            break;
+                        default: break;
+                    }
+                    Console.WriteLine("Wyświetlić lise: (y/n)");
+                    if (Console.ReadLine()[0] == 'y')
+                        lista.ToList().ForEach(x => x.PokazNaKonsoli());
+                    Console.ReadKey();
+                    break;
+                case '3':
+                    Console.WriteLine("Podaj nazwe: ");
+                    string name = Console.ReadLine();
+                    if (name == "" || name == null)
+                        break;
+                    Console.WriteLine("Podaj pilnosc zadania(0-3): ");
+                    int priority = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj date deadline'u");
+                    Console.WriteLine("Dzien: ");
+                    int Day = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Miesiac: ");
+                    int Month = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Rok: ");
+                    int Year = int.Parse(Console.ReadLine());
+
+                    if (Day < 1 || Day > 31)
+                        break;
+                    if (Month < 1 || Month > 12)
+                        break;
+                    if (Year < 2022)
+                        break;
+                    lista.Add(new Item(name, priority, new DateTime(Year, Month, Day)));
+                    Console.WriteLine("Pomyslnie utworzono nowy obiekt!");
+                    Console.ReadKey();
+                    break;
+                case '4':
+                    lista.ToList().ForEach(x => x.PokazNaKonsoli());
+                    Console.WriteLine("Podaj nr zadania ktroe chcesz usunac z listy: ");
+                    int DeleteNum = int.Parse(Console.ReadLine());
+                    lista.RemoveAt(DeleteNum - 1); Console.ReadKey(); break;
+                case '5':
+                    lista.ToList().ForEach(x => x.PokazNaKonsoli());
+                    Console.WriteLine("Podaj nr zadania do zmiany: ");
+                    int doZmiany = int.Parse(Console.ReadLine()) - 1;
+                    if (doZmiany < 0 || doZmiany > lista.Count - 1)
+                        break;
+                    lista[doZmiany].Zmien();
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+}
+
